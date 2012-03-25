8e1315dfdcb3815566088fb1e7dc2dd5a8c469a6
Piti Ongmongkolkul <piti118@gmail.com>
2012-03-25 04:44:16 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup, Extension
+import distutils.util
+import subprocess
+import numpy as np
+root_inc = subprocess.check_output(["root-config", "--incdir"]).strip()
+root_ldflags = subprocess.check_output(["root-config", "--libs"]).strip().split(' ')
+print 'xxx',root_ldflags
+
+print distutils.util.get_platform() 
+module = Extension('root_numpy',
+                    sources = ['src/root_numpy.cc'],
+                    include_dirs= [np.get_include(),root_inc],
+                    #extra_compile_args = []+root_cflags,
+                    extra_link_args = []+root_ldflags)
+
+setup (name = 'root_numpy',
+       version = '1.0',
+       description = 'Convert root tree to numpy array',
+       author='Piti Ongmongkolkul',
+       author_email='piti@caltech.edu',
+       url='https://github.com/piti118/root_numpy',
+       ext_modules = [module]
+       )
---
+++ src/root_numpy.cc
@@ -0,0 +1,278 @@
+#include <Python.h>
+#include <string>
+#include <iostream>
+#include <TTree.h>
+#include <TFile.h>
+#include <TChain.h>
+#include <TLeaf.h>
+#include <map>
+#include <numpy/arrayobject.h>
+#include <cassert>
+#include <set>
+struct TypeInfo{
+    TypeInfo(PyObject* nptype, int size):nptype(nptype),size(size){};
+    PyObject* nptype;
+    int size;//in bytes
+};
+
+static std::map<std::string, TypeInfo> root_typemap;
+
+void init_roottypemap(){
+    //TODO: correct this one so it doesn't depend on system
+    TypeInfo intLike(PyString_FromString("i4"),sizeof(Int_t));
+    TypeInfo floatLike(PyString_FromString("f4"),sizeof(Float_t));
+    TypeInfo doubleLike(PyString_FromString("f8"),sizeof(Double_t));
+    root_typemap.insert(std::make_pair("Int_t",intLike));
+    root_typemap.insert(std::make_pair("Bool_t",intLike));
+    root_typemap.insert(std::make_pair("Float_t",floatLike));
+    root_typemap.insert(std::make_pair("Double_t",doubleLike));
+}
+
+TypeInfo* convert_roottype(const std::string& t){
+    std::map<std::string, TypeInfo>::iterator it = root_typemap.find(t);
+    if(it==root_typemap.end()){
+        std::string msg = "Unknown root type: "+t;
+        PyErr_SetString(PyExc_RuntimeError,msg.c_str());
+        return NULL;
+    }
+    return &(it->second);
+}
+
+struct LeafInfo{
+    std::string name;
+    TypeInfo* type;
+    std::string root_type;
+    char payload[64];//reserve for payload
+    LeafInfo():name(),type(){}
+    LeafInfo(const std::string& name,const std::string& root_type):name(name),root_type(root_type),type(0){};
+    std::string repr(){
+        return name + "("+root_type+")";
+    }
+};
+//return all branch name from tree
+std::vector<std::string> get_branchnames(TTree& tree){
+    TObjArray* branches = (TObjArray*)tree.GetListOfBranches();
+    std::vector<std::string> ret;
+    for(int i=0;i<branches->GetEntries();++i){
+        TBranch* thisBranch = dynamic_cast<TBranch*>(branches->At(i));
+        const char* tmp = thisBranch->GetName();
+        std::string str(tmp);
+        ret.push_back(tmp);
+    }
+    return ret;
+}
+
+//vector unique with order preservation(just get the first one) O(nlogn)
+std::vector<std::string> vector_unique(const std::vector<std::string>& org){
+    using namespace std;
+    set<string> myset;
+    myset.insert(org.begin(),org.end());
+    vector<string> ret;
+    for(int i=0;i<org.size();i++){
+        set<string>::iterator it = myset.find(org[i]);
+        if(it!=myset.end()){
+            myset.erase(it);
+            ret.push_back(org[i]);
+        }
+    }
+    return ret; 
+}
+
+//get list of leafinfo from tree
+//if branches is not empty, only the branches specified in branches will be used
+//otherwise it will automatically list all the branches of the first tree in chain 
+std::vector<LeafInfo*> get_leafinfo(TTree& tree,const std::vector<std::string>& branches){
+    
+    using namespace std;
+    vector<string> branchNames;
+    std::vector<LeafInfo*> ret;
+    if(branches.size()==0) branchNames = get_branchnames(tree);
+    else branchNames = vector_unique(branches);
+
+    for(int i=0;i<branchNames.size();i++){
+        TBranch* thisBranch = dynamic_cast<TBranch*>(tree.GetBranch(branchNames[i].c_str()));
+        std::string roottype("Float_t");
+        if(thisBranch!=0){
+            TObjArray* leaves = thisBranch->GetListOfLeaves();
+            assert(leaves!=0);
+            TLeaf* thisLeaf = dynamic_cast<TLeaf*>(leaves->At(0));
+            assert(thisLeaf!=0);
+            roottype = thisLeaf->GetTypeName();
+        }else{    
+            std::cout << "Warning: branch not found in the first tree(assume type of Float_t)" << branchNames[i] << std::endl;
+        }
+        //need to set branch address at tree level because TChain will fail if branch is set at the first tree
+        LeafInfo* li = new LeafInfo(thisBranch->GetName(),roottype);
+        tree.SetBranchAddress(thisBranch->GetName(),&(li->payload));
+        ret.push_back(li);
+    }
+    return ret;
+}
+//helper function for building numpy descr
+PyObject* build_numpy_descr(const std::vector<LeafInfo*>& lis){
+    
+    PyObject* mylist = PyList_New(0);
+    for(int i=0;i<lis.size();++i){
+        PyObject* pyname = PyString_FromString(lis[i]->name.c_str());
+        TypeInfo* ti = convert_roottype(lis[i]->root_type);
+        lis[i]->type = ti;
+        if(ti==0) return NULL;
+        PyObject* pytype = ti->nptype;
+
+        Py_INCREF(pytype);
+        PyObject* nt_tuple = PyTuple_New(2);
+        PyTuple_SetItem(nt_tuple,0,pyname);
+        PyTuple_SetItem(nt_tuple,1,pytype);
+        
+        PyList_Append(mylist,nt_tuple);
+    }
+    return mylist;
+}
+//convert all leaf specified in lis to numpy structured array
+PyObject* read_helper(TTree& chain, std::vector<LeafInfo*>& lis){
+    int numEntries = chain.GetEntries();
+    PyObject* numpy_descr = build_numpy_descr(lis);
+    if(numpy_descr==0){return NULL;}
+    
+    //build the array
+    PyArray_Descr* descr;
+    PyArray_DescrConverter(numpy_descr,&descr);
+    Py_DECREF(numpy_descr);
+    
+    npy_intp dims[1];
+    dims[0]=numEntries;
+    
+    PyArrayObject* array = (PyArrayObject*)PyArray_SimpleNewFromDescr(1,dims,descr);
+    
+    //assume numpy array is contiguous
+    char* current = (char*)PyArray_DATA(array);
+    //now put stuff in array
+    for(int iEntry=0;iEntry<numEntries;++iEntry){
+        chain.GetEntry(iEntry);
+        for(int ileaf=0;ileaf<lis.size();++ileaf){
+            //cout << *(int*)(lis[ileaf]->payload) << " " << *(float*)(lis[ileaf]->payload) << endl;
+            int size = lis[ileaf]->type->size;
+            memcpy((void*)current,(void*)lis[ileaf]->payload,size);
+            current+=size;
+        }
+    }
+    //return Py_BuildValue("i",numEntries);
+    return (PyObject*)array;
+}
+
+//convert list of string to vector of string
+//if los is just a string vos will be filled with that string
+//if los is null or PyNone it do nothing to vos and return OK;
+int los2vos(PyObject* los, std::vector<std::string>& vos){
+    int ret=1;
+    if(los==NULL){
+        //do nothing
+    }
+    else if(los==Py_None){
+        //do nothing
+    }
+    else if(PyString_Check(los)){
+        char* tmp = PyString_AsString(los);
+        vos.push_back(tmp);
+    }else if(PyList_Check(los)){
+        int len = PyList_Size(los);
+        for(int i=0;i<len;i++){
+            PyObject* s = PyList_GetItem(los,i);
+            if(!s){return NULL;}
+            char* tmp = PyString_AsString(s);
+            if(!tmp){return NULL;}
+            std::string str(tmp);
+            vos.push_back(tmp);
+        }
+    }else{
+        ret=NULL;
+    }
+    return ret;
+}
+
+/**
+* loadTTree from PyObject if fnames is list of string then it load every pattern in fnames
+* if fnames is just a string then it load just one
+* caller is responsible to delete the return value
+* null is return if fnames is not a valid python object(either list of string or string)
+*/
+TTree* loadTree(PyObject* fnames, const char* treename){
+    using namespace std;
+    vector<string> vs;
+    if(!los2vos(fnames,vs)){return NULL;}
+    TChain* chain = new TChain(treename);
+    for(int i=0;i<vs.size();++i){chain->Add(vs[i].c_str());}
+    return dynamic_cast<TTree*>(chain);
+}
+
+PyObject* root2numpy(PyObject *self, PyObject *args, PyObject* keywords){
+    using namespace std;
+    PyObject* fnames;
+    char* treename_;
+    PyObject* branches_=NULL;
+    PyObject* array=NULL;
+    static const char* keywordslist[] = {"fname","treename","branches",NULL};
+    
+    if(!PyArg_ParseTupleAndKeywords(args,keywords,"Os|O",const_cast<char **>(keywordslist),&fnames,&treename_,&branches_)){
+        return NULL;
+    }
+    
+    vector<string> branches;
+    if(!los2vos(branches_,branches)){return NULL;}
+    
+    TTree* chain = loadTree(fnames,treename_);
+    if(!chain){return NULL;}
+    
+    int numEntries = chain->GetEntries();
+    if(numEntries==0){
+        PyErr_SetString(PyExc_TypeError,"Empty Tree");
+        return NULL;
+    }
+    
+    vector<LeafInfo*> lis =  get_leafinfo(*chain,branches);
+    
+
+    
+    array = read_helper(*chain, lis);
+    
+    //don't switch these two lines because lis[i] contains payload
+    delete chain;
+    for(int i=0;i<lis.size();i++){delete lis[i];}
+
+    return (PyObject*)array;
+}
+
+static PyMethodDef methods[] = {
+    {"read",  (PyCFunction)root2numpy, METH_VARARGS|METH_KEYWORDS,
+    "read(fnames,treename,branches=None)\n"
+    "convert tree treename in root files specified in fnames to numpy structured array\n"
+    "------------------\n"
+    "return numpy array\n"
+    "fnames: list of string or string. Root file name patterns. Anything that works with TChain.Add is accepted\n"
+    "treename: name of tree to convert to numpy array\n"
+    "branches(optional): list of string for branch name to be extracted from tree.\n"
+    "\tIf branches is not specified or is none or is empty, all from the first treebranches are extracted\n"
+    "\tIf branches contains duplicate branches, only the first one is used.\n"
+    "\n"
+    "Caveat: This should not matter for most use cases. But, due to the way TChain works, if the trees specified in the input files have different\n"
+    "structure, only the branch in the first tree will be automatically extracted. You can work around this by either reordering the input file or\n"
+    "specify the branches manually.\n"
+    "------------------\n"
+    "Ex:\n"
+    "root_numpy.read('a.root','mytree')#read all branches from tree named mytree from a.root\n\n"
+    "root_numpy.read('a*.root','mytree')#read all branches from tree named mytree from a*.root\n\n"
+    "root_numpy.read(['a*.root','b*.root'],'mytree')#read all branches from tree named mytree from a*.root and b*.root\n\n"
+    "root_numpy.read('a.root','mytree','x')#read branch x from tree named mytree from a.root(useful if memory usage matters)\n\n"
+    "root_numpy.read('a.root','mytree',['x','y'])#read branch x and y from tree named mytree from a.root\n"
+    },
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+PyMODINIT_FUNC
+initroot_numpy(void)
+{
+    import_array();
+    init_roottypemap();
+    (void) Py_InitModule("root_numpy", methods);
+    //import_array();
+}
