0d7299d7a0285652ab87c1162a86228a1cc858d7
Piti Ongmongkolkul <piti118@gmail.com>
2012-03-27 18:01:06 -0700

    actually uint is better when drawing histogram

--- src/croot_numpy.cc
+++ src/croot_numpy.cc
@@ -70,7 +70,9 @@ void init_roottypemap(){
     
     //this one is kinda special currently need to read c-api on exacly how numpy and root store bool
     //but int seems to work
-    root_typemap.insert(make_pair("Bool_t",TypeInfo("bool",1)));  
+    //root_typemap.insert(make_pair("Bool_t",TypeInfo("bool",1)));  
+    root_typemap.insert(make_pair("Bool_t",TypeInfo("u1",1)));  
+
 }
 
 TypeInfo* convert_roottype(const std::string& t){
