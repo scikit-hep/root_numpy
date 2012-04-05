a5762ca649de9bb490cb260ddd2bad80e8c37a23
Piti Ongmongkolkul <piti118@gmail.com>
2012-04-05 14:03:45 -0700

    change directory structure

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ root_inc = subprocess.check_output(["root-config", "--incdir"]).strip()
 root_ldflags = subprocess.check_output(["root-config", "--libs"]).strip().split(' ')
 
 module = Extension('root_numpy.croot_numpy',
-                    sources = ['src/croot_numpy.cc'],
+                    sources = ['root_numpy/croot_numpy.cc'],
                     include_dirs= [np.get_include(),root_inc],
                     #extra_compile_args = []+root_cflags,
                     extra_link_args = []+root_ldflags)
 
 setup (name = 'root_numpy',
-       version = '1.02',
+       version = '1.03',
        description = 'Convert root tree to numpy array',
        author='Piti Ongmongkolkul',
        author_email='piti118@gmail.com',
        url='https://github.com/piti118/root_numpy',
-       package_dir = {'root_numpy': 'src'},
+       package_dir = {'root_numpy': 'root_numpy'},
        packages = ['root_numpy'],
        ext_modules = [module]
        )
