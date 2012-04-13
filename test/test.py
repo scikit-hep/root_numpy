11b91a685113185e518f701226bc2661ac6587f5
Piti Ongmongkolkul <piti118@gmail.com>
2012-04-12 17:43:19 -0700

    delay loading ROOT until it's necessary

--- test/test.py
+++ test/test.py
@@ -2,10 +2,10 @@ from root_numpy import *
 
 #print 'hhhhh'
 
-#a = root2array('test/single.root','tree')
-#a = root2array('test/fixed.root','tree')
-#a = root2array('test/vary.root','tree')
-a = root2array('test/sprtest.root','ClassRecord')
+#a = root2array('test/single*.root','tree')
+#a = root2array('test/fixed*.root','tree')
+a = root2array('test/vary*.root','tree')
+#a = root2array('test/sprtest.root','ClassRecord')
 print a
 print a.dtype
 #print 'kkkk'
--- test/testsample.C
+++ test/testsample.C
@@ -1,42 +1,53 @@
-void makesingle(){
-    TFile file("single.root","RECREATE");
+#include <cstdio>
+#include "TFile.h"
+#include "TTree.h"
+
+void makesingle(int id){
+    char buffer[255];
+    sprintf(buffer,"single%d.root",id);
+    TFile file(buffer,"RECREATE");
     TTree tree("tree","tree");
     int n; tree.Branch("n_int",&n,"n_int/I");
     float f; tree.Branch("f_float",&f,"f_float/F");
     double d; tree.Branch("d_double",&d,"d_double/D");
     for(int i=0;i<100;i++){
-        n = i;
-        f = i*2.0;
-        d = i*3.0;
+        n = i+id;
+        f = i*2.0+id;
+        d = i*3.0+id;
         tree.Fill();
     }
     tree.Write();
+    file.Close();
 }
 
-void makefixed(){
-    TFile file("fixed.root","RECREATE");
+void makefixed(int id){
+    char buffer[255];
+    sprintf(buffer,"fixed%d.root",id);
+    TFile file(buffer,"RECREATE");
     TTree tree("tree","tree");
     
     int n[5]; tree.Branch("n_int",&n,"n_int[5]/I");
     float f[7]; tree.Branch("f_float",&f,"f_float[7]/F");
     double d[10]; tree.Branch("d_double",&d,"d_double[10]/D");
     for(int i=0;i<100;i++){
-        for(int i_n=0;i_n<5;i_n++){n[i_n] = 5*i+i_n;}
-        for(int i_f=0;i_f<7;i_f++){f[i_f] = 2*(5*i+i_f)+0.5;}
-        for(int i_d=0;i_d<10;i_d++){d[i_d] = 3*(5*i+i_d)+0.5;}
+        for(int i_n=0;i_n<5;i_n++){n[i_n] = 5*i+i_n+id;}
+        for(int i_f=0;i_f<7;i_f++){f[i_f] = 2*(5*i+i_f)+0.5+id;}
+        for(int i_d=0;i_d<10;i_d++){d[i_d] = 3*(5*i+i_d)+0.5+id;}
         tree.Fill();
     }
     tree.Write();
 }
 
-void makevary(){
+void makevary(int id){
     int n[100];
     int len_n;
     float f[100];
     int len_f;    
     double d[100];
     int len_d;
-    TFile file("vary.root","RECREATE");
+    char buffer[255];
+    sprintf(buffer,"vary%d.root",id);
+    TFile file(buffer,"RECREATE");
     TTree tree("tree","tree");
     tree.Branch("len_n",&len_n,"len_n/I");
     tree.Branch("len_f",&len_f,"len_f/I");
@@ -46,9 +57,9 @@ void makevary(){
     tree.Branch("d_double",&d,"d_double[len_d]/D");
     
     for(int i=0;i<20;i++){
-        len_n = i;
-        len_f = i+1;
-        len_d = i+2;
+        len_n = i*id;
+        len_f = i*id+1;
+        len_d = i*id+2;
         for(int i_n=0;i_n<len_n;i_n++){n[i_n]=20*i+i_n;}
         for(int i_f=0;i_f<len_f;i_f++){f[i_f]=20*i+2.*i_f+0.5;}
         for(int i_d=0;i_d<len_d;i_d++){d[i_d]=20*i+3.*i_d+0.25;}
@@ -58,7 +69,10 @@ void makevary(){
 }
 
 void testsample(){
-    makesingle();
-    makefixed();
-    makevary();
+    makesingle(1);
+    makesingle(2);
+    makefixed(1);
+    makefixed(2);
+    makevary(1);
+    makevary(2);
 }
