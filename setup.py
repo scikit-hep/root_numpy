c15db1e3a667ebfe58738dd97ef21df7b80b3f22
Noel Dawe <noel.dawe@gmail.com>
2012-11-11 11:47:28 +0100

    use distribute for installation and install test data with package_data

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
 from distutils.core import setup, Extension
 import distutils.util
 import subprocess
 import numpy as np
 import os
+from glob import glob
+
 root_inc = ''
 root_ldflags = []
 try:
-    root_inc = subprocess.Popen(["root-config", "--incdir"], 
+    root_inc = subprocess.Popen(["root-config", "--incdir"],
                         stdout=subprocess.PIPE).communicate()[0].strip()
-    root_ldflags = subprocess.Popen(["root-config", "--libs"], 
+    root_ldflags = subprocess.Popen(["root-config", "--libs"],
                         stdout=subprocess.PIPE).communicate()[0].strip().split(' ')
 except OSError:
     rootsys = os.environ['ROOTSYS']
-    root_inc = subprocess.Popen([rootsys+"/bin/root-config", "--incdir"], 
+    root_inc = subprocess.Popen([rootsys+"/bin/root-config", "--incdir"],
                         stdout=subprocess.PIPE).communicate()[0].strip()
-    root_ldflags = subprocess.Popen([rootsys+"/bin/root-config", "--libs"], 
+    root_ldflags = subprocess.Popen([rootsys+"/bin/root-config", "--libs"],
                         stdout=subprocess.PIPE).communicate()[0].strip().split(' ')
 
 module = Extension('root_numpy._librootnumpy',
@@ -25,23 +32,25 @@ module = Extension('root_numpy._librootnumpy',
                    extra_compile_args = [],
                    extra_link_args=[] + root_ldflags)
 
-setup(name='root_numpy',
-       version='2.00',
-       description='ROOT TTree to numpy array converter',
-       author='Piti Ongmongkolkul',
-       author_email='piti118@gmail.com',
-       url='https://github.com/piti118/root_numpy',
-       download_url='https://github.com/piti118/root_numpy/archive/v2.00.zip',
-       package_dir={'root_numpy': 'root_numpy'},
-       packages=['root_numpy'],
-       ext_modules=[module],
-       classifiers=[
-          "Programming Language :: Python",
-          "Topic :: Utilities",
-          "Operating System :: POSIX :: Linux",
-          "Development Status :: 5 - Production/Stable",
-          "Intended Audience :: Science/Research",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: MIT License",
-        ]
-    )
+setup(
+    name='root_numpy',
+    version='2.00',
+    description='ROOT TTree to numpy array converter',
+    author='Piti Ongmongkolkul',
+    author_email='piti118@gmail.com',
+    url='https://github.com/piti118/root_numpy',
+    download_url='https://github.com/piti118/root_numpy/archive/v2.00.zip',
+    packages=find_packages(),
+    package_data={
+        'root_numpy': ['tests/*.root']},
+    ext_modules=[module],
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
