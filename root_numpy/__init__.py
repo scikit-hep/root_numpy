26acccfd7760c128f8c8724edf03f6fcebff1be8
Noel Dawe <noel.dawe@gmail.com>
2012-11-11 10:36:59 +0100

    pep8 on __init__.py

--- root_numpy/__init__.py
+++ root_numpy/__init__.py
@@ -1,8 +1,20 @@
-__all__ = ['root2array','root2rec','list_trees','list_branches','lt','lst','lb','tree2array','tree2rec']
-__version__ = '2.00'
-import _librootnumpy
 from glob import glob
 import numpy as np
+
+from . import _librootnumpy
+
+__all__ = [
+    'root2array',
+    'root2rec',
+    'list_trees',
+    'list_branches',
+    'lt',
+    'lst',
+    'lb',
+    'tree2array',
+    'tree2rec']
+__version__ = '2.00'
+
 
 def list_trees(fname):
     """list trees in rootfile *fname*"""
@@ -15,7 +27,7 @@ def lt(fname):
 
 
 def list_branches(fname, treename=None):
-    """ 
+    """
     get a list of branches for given *fname* and *treename*.
     *treename* is optional if fname has only one tree
     """
@@ -24,22 +36,22 @@ def list_branches(fname, treename=None):
 
 def lb(fname, treename=None):
     """shorthand for :func:`list_branches`"""
-    return list_branches(fname,treename)
+    return list_branches(fname, treename)
 
 
-def lst(fname,treename=None):
+def lst(fname, treename=None):
     """
     return tree structures.
-    *treename* is optional if fname has only one tree 
+    *treename* is optional if fname has only one tree
     """
-    return _librootnumpy.list_structures(fname,treename)
+    return _librootnumpy.list_structures(fname, treename)
 
 
 def root2array(fnames, treename=None, branches=None, N=None, offset=0):
 
     """
     convert tree *treename* in root files specified in *fnames* to
-    numpy structured array. Type conversion table 
+    numpy structured array. Type conversion table
     is given :ref:`here <conversion_table>`
 
     Arguments:
@@ -50,43 +62,43 @@ def root2array(fnames, treename=None, branches=None, N=None, offset=0):
 
         *treename*: name of tree to convert to numpy array.
         This is optional if the file contains exactly 1 tree.
-        
+
         *branches(optional)*: list of string for branch name to be
         extracted from tree.
 
         * If branches is not specified or is None or is empty,
-          all from the first treebranches are extracted  
+          all from the first treebranches are extracted
         * If branches contains duplicate branches, only the first one is used.
-        
+
         *N(optional)*: maximum number of data that it should load
         useful for testing out stuff
-        
+
         *offset(optional)*: start index (first one is 0)
 
     **Example**
-    
+
     ::
-    
+
         # read all branches from tree named mytree from a.root
         # remember that 'mytree' is optional if a.root has 1 tree
         root2array('a.root', 'mytree')
 
     ::
-    
+
         #read all branches starting from record 5 for 10 records
         #or the end of file.
         root2array('a.root', 'mytree',offset=5,N=10)
-    
+
     ::
 
         # read all branches from tree named mytree from a*.root
         root2array('a*.root', 'mytree')
-    
+
     ::
 
         # read all branches from tree named mytree from a*.root and b*.root
         root2array(['a*.root', 'b*.root'], 'mytree')
-    
+
     ::
 
         #read branch x and y from tree named mytree from a.root
@@ -94,14 +106,14 @@ def root2array(fnames, treename=None, branches=None, N=None, offset=0):
 
 
     .. note::
-    
+
         Due to
         the way TChain works, if the trees specified
         in the input files have different structures, only the
         branch in the first tree will be automatically extracted.
         You can work around this by either reordering the input
         file or specifying the branches manually.
-    
+
     """
     if treename is None:
         afname = None
@@ -116,17 +128,18 @@ def root2array(fnames, treename=None, branches=None, N=None, offset=0):
                              + str(trees))
         else:
             treename = trees[0]
-    
+
     filenames = []
     if isinstance(fnames, basestring):
         filenames = glob(fnames)
     else:
         for fn in fnames:
             tmp = glob(fn)
-            if len(tmp)==0:
-                raise IOError('%s does not match any readble file.'%tmp)
+            if len(tmp) == 0:
+                raise IOError('%s does not match any readble file.' % tmp)
             filenames.extend(tmp)
-    return _librootnumpy.root2array_fromFname(filenames, treename, branches,N,offset)
+    return _librootnumpy.root2array_fromFname(
+        filenames, treename, branches, N, offset)
 
 
 def root2rec(fnames, treename=None, branches=None, N=None, offset=0):
@@ -140,10 +153,10 @@ def root2rec(fnames, treename=None, branches=None, N=None, offset=0):
     .. seealso::
         :func:`root2array`
     """
-    return root2array(fnames, treename, branches,N,offset).view(np.recarray)
+    return root2array(fnames, treename, branches, N, offset).view(np.recarray)
 
 
-def tree2array(tree, branches=None,N=None,offset=0):
+def tree2array(tree, branches=None, N=None, offset=0):
     """
     convert PyROOT TTree *tree* to numpy structured array
     see :func:`root2array` for details on parameter
@@ -154,16 +167,17 @@ def tree2array(tree, branches=None,N=None,offset=0):
 
     if hasattr(ROOT, 'AsCapsule'):
         #o = ROOT.AsCapsule(tree)
-        raise NotImplementedError()#this will cause tons of compilation issue
+        # this will cause tons of compilation issue
+        raise NotImplementedError()
         #return _librootnumpy.root2array_from_capsule(o, branches)
     else:
         o = ROOT.AsCObject(tree)
-        return _librootnumpy.root2array_fromCObj(o, branches,N,offset)
+        return _librootnumpy.root2array_fromCObj(o, branches, N, offset)
 
 
-def tree2rec(tree, branches=None,N=None,offset=0):
+def tree2rec(tree, branches=None, N=None, offset=0):
     """
     convert PyROOT TTree *tree* to numpy structured array
     see :func:`root2array` for details on parameters.
     """
-    return tree2array(tree, branches,N,offset).view(np.recarray)
+    return tree2array(tree, branches, N, offset).view(np.recarray)
