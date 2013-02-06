e059f22d2884a6b209f1f346d9a9d02593f23eb0
Noel Dawe <noel.dawe@gmail.com>
2013-02-06 01:21:46 +0100

    clean up BetterChain.h

--- root_numpy/src/BetterChain.h
+++ root_numpy/src/BetterChain.h
@@ -25,23 +25,6 @@ using namespace std;
 class BetterChain
 {
     public:
-        class MiniNotify: public TObject
-        {
-            public:
-                bool notified;
-                TObject* oldnotify;
-                MiniNotify(TObject* oldnotify):TObject(),notified(false),oldnotify(oldnotify){}
-                virtual Bool_t Notify(){
-                    notified=true;
-                    if(oldnotify) oldnotify->Notify();
-                    return true;
-                }
-        };
-
-        TTree* fChain;
-        int fCurrent;
-        int ientry;
-        MiniNotify* notifier;
 
         BetterChain(TTree* fChain):
             fChain(fChain),
@@ -51,15 +34,17 @@ class BetterChain
             notifier = new MiniNotify(fChain->GetNotify());
             fChain->SetNotify(notifier);
             LoadTree(0);
-            fChain->SetBranchStatus("*",0);//disable all branches
+            // Disable all branches
+            fChain->SetBranchStatus("*", 0);
             //fChain->SetCacheSize(10000000);
         }
 
         ~BetterChain(){
-            if (!fChain) return;//some how i need this(copy from make class)
+            if (!fChain)
+                return; // Somehow i need this (copy from make class)
 
-            fChain->SetNotify(notifier->oldnotify);//do not switch these two lines!
-            //delete fChain->GetCurrentFile();//root does something funny
+            fChain->SetNotify(notifier->oldnotify); // Do not switch these two lines!
+            //delete fChain->GetCurrentFile(); // ROOT does something funny
 
             LeafCache::iterator it;
             for(it=leafcache.begin();it!=leafcache.end();++it){
@@ -69,13 +54,8 @@ class BetterChain
             delete notifier;
         }
 
-        typedef pair<string,string> BL; //branch name to leaf name conversion
-        typedef map<BL,Column*> LeafCache;
-        //column pointer cache since the leaf inside needs to be updated
-        //when new file is loaded in the chain
-        LeafCache leafcache;
-
-        int LoadTree(int entry){
+        int LoadTree(int entry)
+        {
             if (!fChain) return -5;
             //RNHEXDEBUG(fChain->FindBranch("mcLen")->FindLeaf("mcLen"));
             Long64_t centry = fChain->LoadTree(entry);
@@ -91,21 +71,25 @@ class BetterChain
             return centry;
         }
 
-        int GetEntry(int entry){
+        int GetEntry(int entry)
+        {
             // Read contents of entry.
-            if (!fChain) return 0;
+            if (!fChain)
+                return 0;
             LoadTree(entry);
             ientry = entry;
             return fChain->GetEntry(ientry);
         }
 
-        int Next(){
+        int Next()
+        {
             int ret = GetEntry(ientry);
-            ientry++;
+            ++ientry;
             return ret;
         }
 
-        void Notify(){
+        void Notify()
+        {
             //taking care of all the leaves
             //RNDEBUG("NOTIFY");
             LeafCache::iterator it;
@@ -113,44 +97,58 @@ class BetterChain
                 string bname = it->first.first;
                 string lname = it->first.second;
                 TBranch* branch = fChain->FindBranch(bname.c_str());
-                if(branch==0){
+                if (branch==0)
+                {
                     cerr << "Warning cannot find branch " << bname << endl;
                     it->second->skipped = true;
                     continue;
                 }
                 TLeaf* leaf = branch->FindLeaf(lname.c_str());
-                if(leaf==0){
-                    cerr << "Warning cannot find leaf " << lname << " for branch " << bname << endl;
+                if (leaf==0)
+                {
+                    cerr << "Warning cannot find leaf " << lname
+                         << " for branch " << bname << endl;
                     it->second->skipped = true;
                     continue;
                 }
-                it->second->SetLeaf(leaf,true);
+                it->second->SetLeaf(leaf, true);
                 it->second->skipped = false;
             }
         }
 
-        int GetEntries(){
+        int GetEntries()
+        {
             int ret = fChain->GetEntries();
             return ret;
         }
 
-        TBranch* FindBranch(const char* bname){
+        TBranch* FindBranch(const char* bname)
+        {
             return fChain->FindBranch(bname);
         }
 
-        Column* MakeColumn(const string& bname, const string& lname, const string& colname){
-            //as bonus set branch status on all the active branch including the branch that define the length
+        Column* MakeColumn(const string& bname,
+                           const string& lname,
+                           const string& colname)
+        {
+            // As a bonus set branch status on all the active branch
+            // including the branch that define the length
             LoadTree(0);
 
             TBranch* branch = fChain->FindBranch(bname.c_str());
-            if(branch==0){
-                PyErr_SetString(PyExc_IOError,format("Cannot find branch %s",bname.c_str()).c_str());
+            if (branch == 0)
+            {
+                PyErr_SetString(PyExc_IOError,
+                    format("Cannot find branch %s", bname.c_str()).c_str());
                 return 0;
             }
 
             TLeaf* leaf = fChain->FindLeaf(lname.c_str());
-            if(leaf==0){
-                PyErr_SetString(PyExc_IOError,format("Cannot find leaf %s for branch %s",lname.c_str(),bname.c_str()).c_str());
+            if (leaf==0)
+            {
+                PyErr_SetString(PyExc_IOError,
+                    format("Cannot find leaf %s for branch %s", lname.c_str(),
+                           bname.c_str()).c_str());
                 return 0;
             }
 
@@ -162,17 +160,52 @@ class BetterChain
 
             //TODO Does it work if user dont' want length column in the structure?
             TLeaf* leafCount = leaf->GetLeafCount();
-            if(leafCount != 0){
+            if (leafCount != 0)
+            {
                 fChain->SetBranchStatus(leafCount->GetBranch()->GetName(),1);
                 fChain->AddBranchToCache(leafCount->GetBranch(),kTRUE);
             }
 
             BL bl = make_pair(bname,lname);
             Column* ret = Column::build(leaf,colname);
-            if(ret==0){return 0;}
+            if (ret == 0)
+                return 0;
             leafcache.insert(make_pair(bl,ret));
             return ret;
         }
+
+        class MiniNotify: public TObject
+        {
+            public:
+                MiniNotify(TObject* oldnotify):
+                    TObject(),
+                    notified(false),
+                    oldnotify(oldnotify){}
+
+                virtual Bool_t Notify()
+                {
+                    notified = true;
+                    if (oldnotify)
+                        oldnotify->Notify();
+                    return true;
+                }
+
+                bool notified;
+                TObject* oldnotify;
+
+        };
+
+        TTree* fChain;
+        int fCurrent;
+        int ientry;
+        MiniNotify* notifier;
+
+        typedef pair<string,string> BL; //branch name to leaf name conversion
+        typedef map<BL,Column*> LeafCache;
+        //column pointer cache since the leaf inside needs to be updated
+        //when new file is loaded in the chain
+        LeafCache leafcache;
+
 };
 
 #endif
