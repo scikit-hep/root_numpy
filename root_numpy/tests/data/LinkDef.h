b26d9836cbed66b5ae073393150fde58eb4b286d
Noel Dawe <noel.dawe@gmail.com>
2013-02-07 19:55:02 +0100

    make test data generation scripts a compiled executable

---
+++ root_numpy/tests/data/LinkDef.h
@@ -0,0 +1,14 @@
+#include <vector>
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class std::vector<float>+ ;
+#pragma link C++ class std::vector<double>+ ;
+#pragma link C++ class std::vector<long>+ ;
+#pragma link C++ class std::vector<char>+ ;
+#pragma link C++ class std::vector<bool>+ ;
+
+#endif
