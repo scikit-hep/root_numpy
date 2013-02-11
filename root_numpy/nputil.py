ed3cf63a3990b8755f42a012fa3feecea1d3478a
Noel Dawe <noel.dawe@gmail.com>
2013-02-11 12:14:34 -0800

    use NumPy-style docs and PEP8

--- root_numpy/nputil.py
+++ root_numpy/nputil.py
@@ -1,10 +1,15 @@
-__all__ = ['stretch']
-
 import numpy as np
 import numpy.lib.recfunctions as nprf
 
-def _is_array_field(arr,col):
-    return arr.dtype[col]=='O'#for now
+__all__ = [
+    'stretch',
+]
+
+
+def _is_array_field(arr, col):
+    # For now:
+    return arr.dtype[col] == 'O'
+
 
 def stretch(arr, col_names, asrecarray=True):
     """
@@ -24,19 +29,20 @@ def stretch(arr, col_names, asrecarray=True):
     has_scalar_filed = False
     first_array = None
 
-    #construct dtype
+    # Construct dtype
     for c in col_names:
-        if _is_array_field(arr,c):
+        if _is_array_field(arr, c):
             dt.append((c, arr[c][0].dtype))
             has_array_field = True
             first_array = c if first_array is None else first_array
-        else:#assume scalar
+        else:
+            # Assume scalar
             dt.append((c, arr[c].dtype))
             has_scalar_filed = True
 
     if not has_array_field:
-        raise RuntimeError('No array column specified.'
-                           ' What are you trying to do?')
+        raise RuntimeError('No array column specified. '
+                           'What are you trying to do?')
 
     vl = np.vectorize(len)
     len_array = vl(arr[first_array])
@@ -46,18 +52,20 @@ def stretch(arr, col_names, asrecarray=True):
     ret = np.empty(numrec, dtype=dt)
 
     for c in col_names:
-        if _is_array_field(arr,c):
-            #FIXME: this is kinda stupid since it put the stack
-            #some where and copy over to return value
+        if _is_array_field(arr, c):
+            # FIXME: this is kinda stupid since it put the stack
+            # some where and copy over to return value
             stack = np.hstack(arr[c])
-            if len(stack)!= numrec:
-                raise RuntimeError('Array filed length doesn\'t match'
-                    'Expect %d found %d in %s'%(numrec, len(stack), c))
+            if len(stack) != numrec:
+                raise RuntimeError(
+                    'Array filed length doesn\'t match'
+                    'Expect %d found %d in %s' %
+                    (numrec, len(stack), c))
             ret[c] = stack
         else:
-            #FIXME: this is kinda stupid since it put the repeat result
-            #some where and copy over to return value
-            ret[c] = np.repeat(arr[c],len_array)
+            # FIXME: this is kinda stupid since it put the repeat result
+            # some where and copy over to return value
+            ret[c] = np.repeat(arr[c], len_array)
 
     if asrecarray:
         ret = ret.view(np.recarray)
