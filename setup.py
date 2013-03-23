596ba1be2c86b8e64995d78a92b74efdbeff580a
Piti Ongmongkolkul <piti118@gmail.com>
2013-03-23 15:28:08 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ librootnumpy = Extension('root_numpy._librootnumpy',
     extra_compile_args = [],
     extra_link_args=[] + root_ldflags + ['-lTreePlayer'])
 
-librootnumpy = Extension('root_numpy._libarray2root',
+libarray2root = Extension('root_numpy._libarray2root',
     sources=['root_numpy/src/_libarray2root.cpp'],
     include_dirs=[
         np.get_include(),
@@ -66,7 +66,7 @@ setup(
     packages=['root_numpy','root_numpy.tests'],
     package_data={
         'root_numpy': ['tests/data/*.root']},
-    ext_modules=[librootnumpy, libnumpyhist, libinnerjoin],
+    ext_modules=[librootnumpy, libnumpyhist, libinnerjoin, libarray2root],
     classifiers=[
         "Programming Language :: Python",
         "Topic :: Utilities",
