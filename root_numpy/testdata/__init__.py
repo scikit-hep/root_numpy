6ff73c607d55dd1e04f3699c7c92f81084d2eeae
Noel Dawe <noel.dawe@gmail.com>
2013-11-28 16:37:28 +0100

    make test.root the default test file

--- root_numpy/testdata/__init__.py
+++ root_numpy/testdata/__init__.py
@@ -2,11 +2,11 @@ import os
 from pkg_resources import resource_filename
 
 
-def get_filepath(name='single1.root'):
+def get_filepath(name='test.root'):
     return resource_filename('root_numpy', os.path.join('testdata', name))
 
 
-def get_file(name='single1.root'):
+def get_file(name='test.root'):
     import ROOT
     filepath = get_filepath(name)
     if not os.path.isfile(filepath):
