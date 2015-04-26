081bd32051400e201f974bad456266b0e7398629
Noel Dawe <noel.dawe@gmail.com>
2015-04-26 14:48:51 +1000

    travis: include tests where TMVA is absent from the ROOT build

--- ci/install.sh
+++ ci/install.sh
@@ -6,16 +6,22 @@
 
 set -e
 
+# Install gcc 4.8
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
 sudo apt-get -qq install g++-4.8
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+
 sudo apt-get -qq install python-nose python-pip
 pip install coverage coveralls
 
 # Install the ROOT binary
-time wget --no-check-certificate https://copy.com/rtIyUdxgjt7h/ci/root_builds/root${ROOT}_python${TRAVIS_PYTHON_VERSION}_gcc4.8_x86_64.tar.gz
-time tar zxf root${ROOT}_python${TRAVIS_PYTHON_VERSION}_gcc4.8_x86_64.tar.gz
-mv root${ROOT}_python${TRAVIS_PYTHON_VERSION}_gcc4.8_x86_64 root
+build=root${ROOT}_python${TRAVIS_PYTHON_VERSION}_gcc4.8_x86_64
+if [ ! -z ${NOTMVA+x} ]; then
+    build+=_notmva
+fi
+time wget --no-check-certificate https://copy.com/rtIyUdxgjt7h/ci/root_builds/${build}.tar.gz
+time tar zxf ${build}.tar.gz
+mv ${build} root
 source root/bin/thisroot.sh
