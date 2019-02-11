e2aa1d6cca27c3e8e1a01348aba7b91049737f86
Chris Burr <chrisburr@users.noreply.github.com>
2019-02-11 06:08:44 +0000

    Remove "prevent numpy upgrade" workaround

--- setup.py
+++ setup.py
@@ -172,15 +172,6 @@ if has_tmva:
 
 
 def setup_package():
-    # Only add numpy to *_requires lists if not already installed to prevent
-    # pip from trying to upgrade an existing numpy and failing.
-    try:
-        import numpy
-    except ImportError:
-        build_requires = ['numpy']
-    else:
-        build_requires = []
-
     if use_setuptools:
         setuptools_options = dict(
             setup_requires=build_requires,
